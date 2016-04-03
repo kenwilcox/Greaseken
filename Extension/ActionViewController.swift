457e9a64489539ac55d583a5561f378305646cdd
Ken Wilcox <wilcoxk@gmail.com>
2016-04-02 21:04:31 -0600

    Trying to get this to work

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -19,7 +19,9 @@ class ActionViewController: UIViewController {
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { [unowned self] (dict, error) in
-                    // do stuff!
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    print(javaScriptValues)
                 }
             }
         }
