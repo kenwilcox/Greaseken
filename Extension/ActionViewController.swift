0d9ad69e727239eee2370312152889e0e0e23f7b
Ken Wilcox <wilcoxk@gmail.com>
2016-04-03 20:52:54 -0600

    Handling some of the keyboard issues, not sure if I got all of them

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -21,6 +21,10 @@ class ActionViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(ActionViewController.done))
         
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: #selector(ActionViewController.adjustForKeyboard(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(ActionViewController.adjustForKeyboard(_:)), name: UIKeyboardWillChangeFrameNotification, object: nil)
+        
         if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
                 itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { [unowned self] (dict, error) in
@@ -36,6 +40,12 @@ class ActionViewController: UIViewController {
                 }
             }
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        script.setContentOffset(CGPointZero, animated: false)
     }
     
     override func didReceiveMemoryWarning() {
@@ -54,4 +64,21 @@ class ActionViewController: UIViewController {
         self.extensionContext!.completeRequestReturningItems([item], completionHandler: nil)
     }
     
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            script.contentInset = UIEdgeInsetsZero
+        //} else {
+        //    script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
 }
